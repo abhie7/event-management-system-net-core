10448ad876d5ad80b4ce031e5c676055795abf18
Abhiraj Chaudhuri <abhirajchaudhuri@gmail.com>
2024-05-02 02:16:21 +0530

    Add Program.cs and Properties

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using EventManagementSystem.Areas.Identity.Data;
+using EventManagementSystem.Context;
+using EventManagementSystem.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("PGSQL") ?? throw new InvalidOperationException("Connection string 'PGSQL' not found.");
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<IdentityDBContext>(
+    options => options.UseNpgsql(connectionString)
+);
+builder.Services.AddDbContext<EventsDbContext>(
+    options => options.UseNpgsql(connectionString)
+);
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(
+        options =>
+        {
+            options.Password.RequiredUniqueChars = 0;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+        }
+        )
+    .AddEntityFrameworkStores<IdentityDBContext>().AddDefaultTokenProviders();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+app.Run();  
